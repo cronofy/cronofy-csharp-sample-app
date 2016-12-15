e1a2cd42a2fa81e3480f7f5b4a34ad51009c2afc
Harry Boyes <harry.boyes@gmail.com>
2016-12-15 10:18:13 +0000

    Implement logger

---
+++ CronofyCSharpSampleApp/Helpers/LogHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace CronofyCSharpSampleApp
+{
+    public static class LogHelper
+    {
+        public static void Log(string line)
+        {
+            using (StreamWriter w = File.AppendText($"{Environment.CurrentDirectory}/Logs/info.txt"))
+            {
+                w.WriteLine($"{DateTime.Now} - {line}");
+            }
+        }
+    }
+}
